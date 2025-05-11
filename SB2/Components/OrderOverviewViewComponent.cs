8eabc4dd1aca55172131658a708165559a085e6b
Nikoline Brink Kjærsgaard <nbkj54129@edu.ucl.dk>
2025-05-11 17:49:28 +0200

    further experimenting with getting orders to show. Still not showing

---
+++ SB2/Components/OrderOverviewViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SB2.Models.ViewModels;
+using Umbraco.Cms.Infrastructure.Persistence;
+
+public class OrderOverviewViewComponent : ViewComponent
+{
+    private readonly IUmbracoDatabase _db;
+    private readonly IUmbracoDatabaseFactory _databaseFactory;
+
+    public OrderOverviewViewComponent(IUmbracoDatabase db, IUmbracoDatabaseFactory databaseFactory)
+    {
+        _db = db;
+        _databaseFactory = databaseFactory;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var db= _databaseFactory.CreateDatabase();
+        var orders = db.Fetch<Order>("SELECT * FROM Orders ORDER BY Created DESC");
+
+        var model = new AllOrdersViewModel
+        {
+            Orders = orders.Select(x => new OrderListItem
+            {
+                Name = $"Order - {x.Created:yyyyMMddHHmmss}",
+                ClientName = x.ClientName,
+                ClientEmail = x.ClientEmail,
+                SalesPersonName = x.SalespersonName,
+                FilledBy = x.FilledBy,
+                BookingType = x.BookingType
+            }).ToList()
+        };
+
+        return View("_OrderOverview", model);
+    }
+}
+
--- SB2/Controllers/OrderController.cs
+++ SB2/Controllers/OrderController.cs
@@ -94,6 +94,5 @@ public class OrderController : SurfaceController
         }
 
 
-
     }
 }
